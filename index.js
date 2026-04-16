c878d1d972ea6251796905a62376db1a4d1aa342
abhishek <abhisheky0718@gmail.com>
2026-04-16 11:11:02 +0530

    updated projrct

--- index.js
+++ index.js
@@ -2,24 +2,26 @@ const choices = ["rock", "paper" , "scissor"];
 const playerdisplay = document.getElementById("playerdisplay");
 const computerdisplay = document.getElementById("computerdisplay");
 const resultdisplay = document.getElementById("resultdisplay");
-
+const playerscoredisplay = document.getElementById("playerscoredisplay");
+let playerscore = 0 ;
+let computerscore = 0 ;
 
 function playgame(playerchoice){
     const computerchoice = choices[Math.floor (Math.random()*3)];
     let result = "";
     if (playerchoice === computerchoice){
-        result = " it's  a tie ⚖️"
+        result = " TIE⚖️"
     }
     else{
         switch(playerchoice){
             case "rock":
-          result =  (computerchoice ==="scissor") ? " YOU WON✌️ " : "SORRY! YOU LOOSE 👎"
+          result =  (computerchoice ==="scissor") ? "YOU WON" : "YOU LOSE"
           break;
           case "paper":
-          result =  (computerchoice ==="rock") ?  " YOU WON ✌️" : "SORRY! YOU LOOSE 👎"
+          result =  (computerchoice ==="rock") ?  "YOU WON" : "YOU LOSE"
           break;
           case "scissor":
-          result =  (computerchoice ==="paper") ?  " YOU WON ✌️" : "SORRY! YOU LOOSE 👎"
+          result =  (computerchoice ==="paper") ?  "YOU WON" : "YOU LOSE"
           break;
         }
     }
@@ -27,13 +29,17 @@ function playgame(playerchoice){
   playerdisplay.textContent = `Player: ${playerchoice}`;
   computerdisplay.textContent = `Computer: ${computerchoice}`;
   resultdisplay.textContent = result;
-
+resultdisplay.classList.remove("green", "red");
   switch(result){
-    case " YOU WON✌️ ":
+    case "YOU WON":
     resultdisplay.style.color = "green";
+    playerscore++;
+    playerscoredisplay.textContent = playerscore ;
     break;
-    case "SORRY! YOU LOOSE 👎":
+    case "YOU LOSE":
     resultdisplay.style.color = "red";
+    computerscore++;
+    computerscoredisplay.textContent = computerscore;
     break;
     default:
     resultdisplay.style.color = "black";
